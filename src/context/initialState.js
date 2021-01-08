2eb2b022b81ccd0eeb0b2f4fb7959ca3c460aef6
Binay <binayprogramming@gmail.com>
2021-01-08 14:35:08 +0100

    Made seperate file for CONTEXT API's files, like actiontypes,intialstate and reducer

---
+++ src/context/initialState.js
@@ -0,0 +1,4 @@
+export const initialState = {
+  jobs: [],
+  loading: true,
+};
--- src/hooks/useFetchJobs.js
+++ src/hooks/useFetchJobs.js
@@ -1,3 +1,12 @@
-const useFetchJobs = (params, page) => {};
+import axios from 'axios';
+import { useReducer } from 'react';
+
+import { initialState } from '../context/initialState';
+import { actionTypes } from '../context/actionTypes';
+import reducer from '../context/reducer';
+
+const useFetchJobs = (params, page) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+};
 
 export default useFetchJobs;
