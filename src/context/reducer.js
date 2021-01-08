1d1e0a952bef9868b6444204563bdb66b17e92d7
Binay <binayprogramming@gmail.com>
2021-01-08 14:42:22 +0100

    Reducer also made in its correspondant file

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,1 +1,19 @@
-export default function reducer(state, action) {}
+import { actionTypes } from './actionTypes';
+
+export default function reducer(state, action) {
+  switch (action.type) {
+    case actionTypes.MAKE_REQUEST:
+      return { loading: true, jobs: [] };
+    case actionTypes.GET_DATA:
+      return { ...state, loading: false, jobs: action.payload.jobs };
+    case actionTypes.ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        jobs: [],
+      };
+    default:
+      return state;
+  }
+}
